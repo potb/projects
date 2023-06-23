c26381f2232eb5c7798604a4fd34b5bb589a52a6
Peïo Thibault <peio.thibault@gmail.com>
2023-06-23 16:36:58 +0200

    migrate aahh

---
+++ apps/aahh/src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  const aahh = async (): Promise<void> => {
+    const random = Math.floor(Math.random() * 2);
+
+    await new Audio(`/aahh_${random}.mp3`).play();
+  };
+</script>
+
+<div>
+  <h1>aahh</h1>
+  <button on:click={aahh}>
+    <img src="/philippe.png" alt="Philippe Etchebest with his mouth open" />
+  </button>
+</div>
+
+<style>
+  button {
+    border: none;
+    background: none;
+    cursor: pointer;
+  }
+</style>
