671c10b253f399572e7294fc314a5ba8088e2555
Peïo Thibault <peio.thibault@gmail.com>
2023-06-26 17:03:43 +0200

    typings

--- apps/aahh/src/app.d.ts
+++ apps/aahh/src/app.d.ts
@@ -1,21 +1,22 @@
+/// <reference types="vite/client" />
+/// <reference types="vite-plugin-pwa/info" />
+
+declare module '*?meta' {
+  export const src: string;
+
+  export const width: number;
+
+  export const height: number;
+}
+
+declare module '*&meta' {
+  export const src: string;
+
+  export const width: number;
+
+  export const height: number;
+}
+
 declare global {
-  import 'vite/client';
-
   namespace App {}
-
-  declare module '*?meta' {
-    export const src: string;
-
-    export const width: number;
-
-    export const height: number;
-  }
-
-  declare module '*&meta' {
-    export const src: string;
-
-    export const width: number;
-
-    export const height: number;
-  }
 }
--- apps/aahh/vite.config.ts
+++ apps/aahh/vite.config.ts
@@ -1,7 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { imagetools } from 'vite-imagetools';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig({
-  plugins: [sveltekit(), imagetools()],
+  plugins: [
+    sveltekit(),
+    imagetools(),
+    SvelteKitPWA({ includeManifestIcons: true }),
+  ],
 });
